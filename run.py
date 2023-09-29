d64397faaca0001310607125f5eb0bd3aefed3df
3rd-W <maheshpanwar351@gmail.com>
2023-09-29 15:16:46 +0530

    commit

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-from myapp import app
+# from myapp import app
+# if __name__ == '__main__':
+#     app.run(debug=False)
 
-if __name__ == '__main__':
-    app.run(debug=False)
+from myapp import create_web
+
+app = create_web()
+
